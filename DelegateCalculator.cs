05010cb3727f645d9ea50aca66c39e8aae343866
XanderChua <72645612+XanderChua@users.noreply.github.com>
2021-09-22 18:11:17 +0800

    Add files via upload

---
+++ DelegateCalculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DelegateCalculator
+{
+    public delegate void EventHandler(int add, int min, int x, int div);
+    class Calculator //publisher
+    {
+        public event EventHandler send;
+        int a;
+        int b;
+        int add;
+        int min;
+        int x; 
+        int div;
+        public void GetInput(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            delegateAdd();
+            delegateMin();
+            delegateX();
+            delegateDiv();
+            Notify();
+        }
+        public void delegateAdd()
+        {
+            add = a + b;
+        }
+        public void delegateMin()
+        {
+            min = a - b;
+        }
+        public void delegateX()
+        {
+            x = a * b;
+        }
+        public void delegateDiv()
+        {
+            div = a / b;
+        }
+        private void Notify()
+        {
+            if(send != null)
+            {
+                send(add, min, x, div);
+            }
+        }
+    }
+    class DelegateCalculator //subscriber
+    {     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Delegate Calculator--");
+            print();
+        }
+        public static void print()
+        {
+            Console.WriteLine("Enter 1st number:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number:");
+            int b = Int32.Parse(Console.ReadLine());
+            Calculator cal = new Calculator();
+            cal.send += Calculator_performAddEvent;
+            cal.GetInput(a, b);
+            Console.ReadLine();
+        }
+        private static void Calculator_performAddEvent(int add, int min, int x, int div)
+        {
+            Console.WriteLine("Addition: " + add);
+            Console.WriteLine("Subtraction: " + min);
+            Console.WriteLine("Multiplication: " + x);
+            Console.WriteLine("Division: " + div);
+        }
+    }
+}
---
+++ DelegateHCFandLCM.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace DelegateHCFandLCM
+{
+    public delegate void EventHandler(int HCF, int LCM);
+    class Operation
+    {
+        public event EventHandler send;
+        int a;
+        int b;
+        int hcf;
+        int lcm;
+
+        public void GetInput(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            calculatehcf();
+            calculatelcm();
+            Notify();
+        }
+        private void calculatehcf()
+        {
+            for (int i = 1; i <= a || i <= b; ++i)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    hcf = i;
+                }
+            }
+        }
+        public void calculatelcm()
+        {
+            lcm = (a * b) / hcf; 
+        }
+        private void Notify()
+        {
+            if (send != null)
+            {
+                send(hcf, lcm);
+            }
+        }
+    }
+    class DelegateHCFandLCM
+    {
+        public static void print()
+        {
+            Console.WriteLine("Enter 1st input:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd input:");
+            int b = Int32.Parse(Console.ReadLine());
+            Operation op = new Operation();
+            op.send += Op_send;
+            op.GetInput(a, b);
+            Console.ReadLine();
+        }
+
+        private static void Op_send(int HCF, int LCM)
+        {
+            Console.WriteLine("HCF: "+ HCF);
+            Console.WriteLine("LCM: "+LCM);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the Highest Common Factor and Lowest Common Multiple using Delegate Events");
+            print();
+        }
+    }
+}
